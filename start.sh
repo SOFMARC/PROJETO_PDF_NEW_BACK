1a83fde8833d81bfbf8f18a0c17551e64252e2c4
wilsonjuniordevelop <126985389+wilsonjuniordevelop@users.noreply.github.com>
2023-06-10 08:43:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 redis-server &
 
 # Inicie o worker do Celery
-celery -A src.Interface.Api.celery_worker.celery worker --loglevel=info &
+celery -A src.Interface.Api.celery_worker.celery worker --concurrency=8 --loglevel=info &
 
 # Inicie o aplicativo Python
 python3 -m src.Interface.Api.api
